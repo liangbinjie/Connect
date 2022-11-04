0fcdc98b10511a4f81bc0270564ba96de87ee1d3
Benji <67171031+liangbinjie@users.noreply.github.com>
2022-11-04 00:06:25 -0600

    Update __init__.py

--- connectApp/__init__.py
+++ connectApp/__init__.py
@@ -13,8 +13,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpeg', 'jpg'}
 
 def create_app():
     app = Flask(__name__)
-    # app.config['SECRET_KEY'] = "os.environ['SECRET_KEY']"
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SECRET_KEY'] = "os.environ['SECRET_KEY']"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     db.init_app(app)
