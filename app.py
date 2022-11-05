c9a9397bf082d1484a5756522c8dc8617eb7b67e
Benji <67171031+liangbinjie@users.noreply.github.com>
2022-11-04 18:22:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from connectApp import create_app
+from connectApp import create_app, db
 import os
 
 app = create_app()
+db.create_all(app=app)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=os.environ("$POST"))
